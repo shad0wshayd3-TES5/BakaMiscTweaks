b2e02ef3766dcb5426af0129b19e7de9d9a51f86
shad0wshayd3 <2724172+shad0wshayd3@users.noreply.github.com>
2026-08-22 22:05:21 -0600

    feat: disable empty folder creation

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,30 @@
+namespace Hooks
+{
+	class hkCreateDirectory_Mods
+	{
+	private:
+		static std::int32_t CreateDirectory(const char* a_name, void*)
+		{
+			if (!_stricmp(a_name, "Creations") || !_stricmp(a_name, "Mods"))
+				return 0;
+			return _CreateDirectory(a_name, nullptr);
+		}
+
+		inline static REL::THook _CreateDirectory{ REL::ID(443417), 0x06, CreateDirectory };
+	};
+
+	class hkCreateDirectory_ShaderCache
+	{
+	private:
+		static std::int32_t CreateDirectory(const char*, void*)
+		{
+			return 0;
+		}
+
+		inline static REL::THook _CreateDirectory{ REL::ID(77226), 0x9DE, CreateDirectory };
+	};
+}
+
 namespace Tweaks
 {
 	class hkMagicEffectDescription
